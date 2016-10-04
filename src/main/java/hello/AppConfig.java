0de62b01339a98b2be79cf2e928b7304807bc6df
Caroline Moers <Caroline_Moers@gap.com>
2016-10-04 10:47:07 -0300

    Initial commit

---
+++ src/main/java/hello/AppConfig.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public AtomicLong counter() {
+        return new AtomicLong();
+    }
+}
